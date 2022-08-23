1b1a1d3805e3c513d91e8dacab2dbb2de3f85ba9
Benjamin Virgin <benjamincvirgin@gmail.com>
2022-08-23 17:09:07 -0600

    Beaming it up, scotty!  Initial commit

---
+++ Assets/app.js
@@ -0,0 +1,83 @@
+var profilePic = document.querySelector('#profilePic');
+
+profilePic.addEventListener('mouseenter', function() {
+  if (!profilePic.classList.contains('hover')) {
+    profilePic.classList.add('hover');
+  }
+})
+
+profilePic.addEventListener('mouseleave', function() {
+  if (profilePic.classList.contains('hover')) {
+    profilePic.classList.remove('hover');
+  }
+})
+
+var wFGImage = document.querySelector('#wFGImage');
+
+wFGImage.addEventListener('mouseenter', function() {
+  if (!wFGImage.classList.contains('hover')) {
+    wFGImage.classList.add('hover');
+  }
+})
+
+wFGImage.addEventListener('mouseleave', function() {
+  if (wFGImage.classList.contains('hover')) {
+    wFGImage.classList.remove('hover');
+  }
+})
+
+var tPImage = document.querySelector('#tPImage');
+
+tPImage.addEventListener('mouseenter', function() {
+  if (!tPImage.classList.contains('hover')) {
+    tPImage.classList.add('hover');
+  }
+})
+
+tPImage.addEventListener('mouseleave', function() {
+  if (tPImage.classList.contains('hover')) {
+    tPImage.classList.remove('hover');
+  }
+})
+
+var lDSImage = document.querySelector('#lDSImage');
+
+lDSImage.addEventListener('mouseenter', function() {
+  if (!lDSImage.classList.contains('hover')) {
+    lDSImage.classList.add('hover');
+  }
+})
+
+lDSImage.addEventListener('mouseleave', function() {
+  if (lDSImage.classList.contains('hover')) {
+    lDSImage.classList.remove('hover');
+  }
+})
+
+var codeXAcademyImage = document.querySelector('#codeXAcademyImage');
+
+codeXAcademyImage.addEventListener('mouseenter', function() {
+  if (!codeXAcademyImage.classList.contains('hover')) {
+    codeXAcademyImage.classList.add('hover');
+  }
+})
+
+codeXAcademyImage.addEventListener('mouseleave', function() {
+  if (codeXAcademyImage.classList.contains('hover')) {
+    codeXAcademyImage.classList.remove('hover');
+  }
+})
+
+var bYUIImage = document.querySelector('#bYUIImage');
+
+bYUIImage.addEventListener('mouseenter', function() {
+  if (!bYUIImage.classList.contains('hover')) {
+    bYUIImage.classList.add('hover');
+  }
+})
+
+bYUIImage.addEventListener('mouseleave', function() {
+  if (bYUIImage.classList.contains('hover')) {
+    bYUIImage.classList.remove('hover');
+  }
+})
