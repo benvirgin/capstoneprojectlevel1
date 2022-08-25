8077e8a20574bba8871312d068cf3e4e81465bb6
Benjamin Virgin <benjamincvirgin@gmail.com>
2022-08-25 10:46:57 -0600

    Changes from eval  Changes to be committed: 	modified:   Assets/app.js 	modified:   Assets/styles.css

--- Assets/app.js
+++ Assets/app.js
@@ -3,15 +3,11 @@
 var profilePic = document.querySelector('#profilePic');
 
 profilePic.addEventListener('mouseenter', function() {
-  if (!profilePic.classList.contains('hover')) {
     profilePic.classList.add('hover');
-  }
 })
 
 profilePic.addEventListener('mouseleave', function() {
-  if (profilePic.classList.contains('hover')) {
     profilePic.classList.remove('hover');
-  }
 })
 
 // Body Images
@@ -19,55 +15,39 @@ profilePic.addEventListener('mouseleave', function() {
 var firepreneurshipImage = document.querySelector('#firepreneurshipImage');
 
 firepreneurshipImage.addEventListener('mouseenter', function() {
-  if (!firepreneurshipImage.classList.contains('hover')) {
     firepreneurshipImage.classList.add('hover');
-  }
 })
 
 firepreneurshipImage.addEventListener('mouseleave', function() {
-  if (firepreneurshipImage.classList.contains('hover')) {
     firepreneurshipImage.classList.remove('hover');
-  }
 })
 
 var aptimaizImage = document.querySelector('#aptimaizImage');
 
 aptimaizImage.addEventListener('mouseenter', function() {
-  if (!aptimaizImage.classList.contains('hover')) {
     aptimaizImage.classList.add('hover');
-  }
 })
 
 aptimaizImage.addEventListener('mouseleave', function() {
-  if (aptimaizImage.classList.contains('hover')) {
     aptimaizImage.classList.remove('hover');
-  }
 })
 
 var codeXAcademyImage = document.querySelector('#codeXAcademyImage');
 
 codeXAcademyImage.addEventListener('mouseenter', function() {
-  if (!codeXAcademyImage.classList.contains('hover')) {
     codeXAcademyImage.classList.add('hover');
-  }
 })
 
 codeXAcademyImage.addEventListener('mouseleave', function() {
-  if (codeXAcademyImage.classList.contains('hover')) {
     codeXAcademyImage.classList.remove('hover');
-  }
 })
 
 var bYUIImage = document.querySelector('#bYUIImage');
 
 bYUIImage.addEventListener('mouseenter', function() {
-  if (!bYUIImage.classList.contains('hover')) {
     bYUIImage.classList.add('hover');
-  }
 })
 
 bYUIImage.addEventListener('mouseleave', function() {
-  if (bYUIImage.classList.contains('hover')) {
     bYUIImage.classList.remove('hover');
-  }
 })
